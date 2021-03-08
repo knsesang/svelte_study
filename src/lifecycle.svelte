ee558c8afc99354f819def41cd140e3a50f93bcf
LeeYongKyu <knsesang2000@gmail.com>
2021-03-08 09:08:51 +0900

    [UPDATE] lifecycle module

---
+++ src/lifecycle.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { lifeCycle, delayRender } from './lifecycle.js';
+	import Something_2 from './something_2.svelte';
+
+	let done = delayRender();
+	lifeCycle();
+</script>
+
+{#if $done}
+	<h1>라이프 사이클</h1>
+{/if}
+
+<Something_2 />
