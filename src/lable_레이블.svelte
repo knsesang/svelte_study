105a1392d6c2739ae81035e266d18085ab185c6f
LeeYongKyu <knsesang2000@gmail.com>
2021-03-08 14:31:20 +0900

    [ADD] lable 레이블

---
+++ src/lable_레이블.svelte
@@ -0,0 +1,24 @@
+<script>
+	loop1: for (let i = 1; i < 4; i++) {
+		loop2: for (let k = 1; k < 4; k++) {
+			if (i === 2) {
+				break loop1;
+			}
+			console.log(`${i}-${k}`);
+		}
+		console.log('---------------');
+	}
+
+	heropy: {
+		console.log('a');
+		break heropy;
+		console.log('b');
+	}
+
+	// svelte 반응성 구문
+
+	let count = 0;
+	$: {
+		console.log(count);
+	}
+</script>
