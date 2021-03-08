d029ad26ddd14e1398c71be289f6f054fd310f7e
LeeYongKyu <knsesang2000@gmail.com>
2021-03-08 16:41:31 +0900

    클래스 바인딩 패턴 정리

---
+++ src/클래스 바인딩 패턴.svelte
@@ -0,0 +1,44 @@
+<script>
+	let active = false;
+	let color = 'orange';
+	let white = 'white';
+
+	let color2 = {
+		t: 'tomato',
+		w: '#FFF',
+	};
+
+	let letterSpacing = 'letter-spacing:5px';
+</script>
+
+<button
+	on:click={() => {
+		active = !active;
+	}}>toggle</button
+>
+<div class={active ? 'active' : ''}>hello</div>
+
+<!-- class 지시어 -->
+<div class:hello={active}>hi</div>
+
+<h2 style="background-color:{color};color:{white}">heropy</h2>
+
+<h3 style="background-color:{color2.t};color:{color2.w}">heropy blog</h3>
+
+<style>
+	div {
+		width: 100px;
+		height: 100px;
+		background-color: red;
+		transition: 0.4s;
+	}
+	.active {
+		width: 200px;
+		background-color: orange;
+	}
+
+	.hello {
+		width: 300px;
+		background-color: yellow;
+	}
+</style>
---
+++ src/클래스_바인딩_패턴_정리.svelte
@@ -0,0 +1,30 @@
+<!-- 
+	클래스 바인딩 패턴 정리
+	class 명은 대부분 소문자로 사용한다
+	바인딩시에 소스 단축을 위해서 소문자 사용 추천 
+-->
+<script>
+	let active = true;
+	let vaild = false;
+	let camelCase = true;
+
+	function multi() {
+		return 'active multiple-class';
+	}
+</script>
+
+<div class={active ? 'active' : ''}>3 항 연산자 보간</div>
+
+<!-- 
+	<div class:active={active}>class 지시어 (directive ) 바인딩</div> 
+	아래와 같은 문법
+-->
+<div class:active>class 지시어 (directive ) 바인딩</div>
+
+<div class="active">class 지시어 바인딩 단축 형태</div>
+
+<div class:active class:vaild class:camelCase class:camel-case={camelCase}>
+	다중 class 지시어 바인딩
+</div>
+
+<div class={multi()}>함수실행</div>
