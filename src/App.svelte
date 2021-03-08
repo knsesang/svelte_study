ad9d45c150f7bc6caff0c6a199d40e351b2dcf95
LeeYongKyu <knsesang2000@gmail.com>
2021-03-08 10:25:14 +0900

    [ADD] 보간

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,33 @@
 <script>
-	import { tick } from 'svelte';
-
-	let name = 'world';
-
-	// 함수안에서 실행중일때는 안에 모든 내용을 실행한후에 화면을 갱신한다
-	// 그러므로  name='heropy'를 설정했다고 해도
-	// h1.innerText 는 변경전의 값인 'world'를 불러올수밖에 없다
-
-	//	이를 위해서 tick을 사용함. asp의  flush  같은 느낌
-	//	비동기이므로 await 를 추가해야 함
-
-	async function handler() {
-		name = 'heropy';
-		await tick();
-		const h1 = document.querySelector('h1');
-		console.log(h1);
-	}
+	let href = 'https://heropy.blog';
+	let name = 'heropy';
+	let value = 'new input value';
+	let isUpperCase = false;
 </script>
 
-<h1 on:click={handler}>{name}</h1>
+<!-- 3개 모두 같은 표현 방식 -->
+<a href="https://heropy.blog">heropy</a> <br />
+
+<!-- 보간 한다고 표현 -->
+<!-- 
+	<a href={href}>{name}</a> <br />
+	▼ 
+-->
+<a {href}>{name}</a> <br />
+
+<!-- <input type="text" value="default value" /> -->
+<!-- 
+	<input {value} on:input={(e) => {value = e.target.value}} /> 
+	▼ 
+-->
+<input {value} on:input={e => (value = e.target.value)} />
+
+<!-- 
+	<input bind:value={value} />
+	▼ 
+-->
+<input bind:value />
+
+<div>
+	{isUpperCase ? 'DIV' : 'div'}
+</div>
