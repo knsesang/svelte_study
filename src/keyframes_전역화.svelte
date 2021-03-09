34b78c04beb787d3b89457048ab9a8ce6e60f4b4
LeeYongKyu <knsesang2000@gmail.com>
2021-03-09 11:22:41 +0900

    @keyframes_전역화

---
+++ src/keyframes_전역화.svelte
@@ -0,0 +1,37 @@
+<!-- @keyframes  전역화 
+	@keyframes zoom {}	일반
+	@keyframes -global-zoom {}	전역화
+
+-->
+<script>
+</script>
+
+<div class="box" />
+
+<style>
+	/* 
+		body {padding: 60px;}		 
+		이 콤포넌트에는  body 가 없으므로 작동안한다	
+	*/
+
+	:global(body) {
+		padding: 60px;
+	}
+
+	.box {
+		width: 100px;
+		height: 100px;
+		background-color: tomato;
+		border-radius: 10px;
+		animation: zoom 0.4s infinite alternate;
+	}
+
+	@keyframes -global-zoom {
+		0% {
+			transform: scale(1);
+		}
+		100% {
+			transform: scale(1.5);
+		}
+	}
+</style>
