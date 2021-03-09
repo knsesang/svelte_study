bf2b868c11f1ed7e25af3039861acb845629d5c4
LeeYongKyu <knsesang2000@gmail.com>
2021-03-09 11:37:41 +0900

    [ADD] 일반요소_바인딩

---
+++ src/일반요소_바인딩.svelte
@@ -0,0 +1,20 @@
+<!-- 일반요소 바인딩 -->
+<script>
+	import { tick } from 'svelte';
+
+	let isShow = false;
+
+	async function toggle() {
+		isShow = !isShow;
+		await tick();
+
+		const inputEl = document.querySelector('input');
+		inputEl && inputEl.focus();
+	}
+</script>
+
+<button on:click={toggle}>수정</button>
+
+{#if isShow}
+	<input />
+{/if}
