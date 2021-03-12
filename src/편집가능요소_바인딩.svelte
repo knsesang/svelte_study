5841460302b2612a7ab60a38685f2eabf4dcf5f3
LeeYongKyu <knsesang2000@gmail.com>
2021-03-12 13:30:03 +0900

    편집가능요소_바인딩

---
+++ src/편집가능요소_바인딩.svelte
@@ -0,0 +1,28 @@
+<!-- 편집가능 요소 바인딩 -->
+<script>
+	let innerHTML = '';
+	let textContent = 'hello world23';
+</script>
+
+<!-- contenteditable tag를 수정 가능하게 만들어줌 -->
+<!-- 
+아래는 출력되지만
+<div contenteditable>hello world</div>
+아래는 출력이 안된다
+<div contenteditable bind:innerHTML bind:textContent>hello world</div> -->
+
+1:
+<div contenteditable bind:innerHTML bind:textContent />
+2:
+<div>{innerHTML}</div>
+3:
+<div>{textContent}</div>
+4:
+<div>{@html innerHTML}</div>
+
+<style>
+	div {
+		border: 1px solid red;
+		margin-bottom: 10px;
+	}
+</style>
