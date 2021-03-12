8127addd139c9a4cf9564cd56b25d6b45f68b0b7
LeeYongKyu <knsesang2000@gmail.com>
2021-03-12 16:49:01 +0900

    키블록

---
+++ src/키블록.svelte
@@ -0,0 +1,19 @@
+<!-- 키블록 -->
+<script>
+	import Count from './Count.svelte';
+	let reset = false;
+</script>
+
+<!-- {#key 연결할_데이타명} -->
+<!-- 연결할_데이타가 변경이 되면 블럭안에 내용을 파괴하고 다시 생성 -->
+
+{#key reset}
+	<Count />
+	<ul>
+		<li />
+		<li />
+		<li />
+	</ul>
+{/key}
+
+<button on:click={() => (reset = !reset)}> reset</button>
